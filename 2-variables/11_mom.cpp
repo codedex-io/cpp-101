b1ba06a0c49e73281a35d2369f2cf63e9e3d2f77
xin ying <m001720@permatapintar.ukm.edu.my>
2024-10-09 22:56:59 +0800

    Create 11_mom.cpp

---
+++ 2-variables/11_mom.cpp
@@ -0,0 +1,17 @@
+// Month-over-month (MoM) 📊
+
+#include <iostream>
+
+int main() {
+    double month1;
+    double month2;
+
+    std::cout << "Enter month1: ";
+    std::cin >> month1;
+
+    std::cout << "Enter month2: ";
+    std::cin >> month2;
+
+    double MoM = (month2 - month1) / month1 * 100;
+    std::cout << "\nThe MoM change is " << MoM << "%";
+}
