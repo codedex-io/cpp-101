68ad1025701a870a61d59a5f4b52eac2d3250cd6
choice404 <choice404@protonmail.com>
2025-03-26 10:19:03 -0700

    Removed unecessary line from the solution for exercise 24.

--- 5-vectors/24_survival_gear.cpp
+++ 5-vectors/24_survival_gear.cpp
@@ -4,19 +4,15 @@
 #include <iostream>
 #include <vector>
 
-int main() { 
-  int pin = 0; 
-
-  std::vector<std::string> gears = {
-    "⛺️ tent",
-    "🏹 bows & arrows", 
-    "🎣 fishing kit", 
-    "🪓 axe", 
-    "🪚 saw",
-    "🔪 knife",
-    "🥫 canned food" 
-    "🧼 soap", 
-    "🧂 salt", 
-    "🥘 pot"
-  };
+int main() {
+  std::vector<std::string> gears = {"⛺️ tent",
+                                    "🏹 bows & arrows",
+                                    "🎣 fishing kit",
+                                    "🪓 axe",
+                                    "🪚 saw",
+                                    "🔪 knife",
+                                    "🥫 canned food"
+                                    "🧼 soap",
+                                    "🧂 salt",
+                                    "🥘 pot"};
 }
