70d14ca2bf7844877687625cb321c4502928c2c5
Sonny Li <sonnynomnom@gmail.com>
2025-03-26 14:59:18 -0400

    Update 28_main.cpp

--- 5-vectors/28_main.cpp
+++ 5-vectors/28_main.cpp
@@ -8,7 +8,7 @@ int main() {
   std::vector<int> num = {2, 4, 3, 6, 1, 9, 8, 0, 8, 5, 7, 3, 3};
 
   int sum = 0;
-  int product = 0;
+  int product = 1;
 
   for (int i = 0; i < num.size(); i++) {
     if (num[i] % 2 == 0) {
