19cbe8cd9e632bcaab30a40fa14b25095ef9d1e8
choice404 <choice404@protonmail.com>
2025-03-26 10:47:04 -0700

    Updated the product variable in exercise 28 to equal 1

--- 5-vectors/28_main.cpp
+++ 5-vectors/28_main.cpp
@@ -8,13 +8,12 @@ int main() {
   std::vector<int> num = {2, 4, 3, 6, 1, 9, 8, 0, 8, 5, 7, 3, 3};
 
   int sum = 0;
-  int product = 0;
+  int product = 1;
 
   for (int i = 0; i < num.size(); i++) {
     if (num[i] % 2 == 0) {
       sum = sum + num[i];
-    }
-    else {
+    } else {
       product = product * num[i];
     }
   }
