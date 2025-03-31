092611288b9ee6ba00e2cfa8b664a0972dfd4577
Sonny Li <sonnynomnom@gmail.com>
2025-03-31 03:01:34 -0400

    Update 22_99_bottles.cpp

--- 4-loops/22_99_bottles.cpp
+++ 4-loops/22_99_bottles.cpp
@@ -4,7 +4,7 @@
 #include <iostream>
 
 int main() {
-  for (int i = 99; i >= 0; i--) {
+  for (int i = 99; i > 0; i--) {
     std::cout << i << " bottles of beer on the wall\n";
     std::cout << i << " bottles of beer\n";
     std::cout << "Take one down, pass it around\n";
